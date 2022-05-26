53459ee2b11e5b100369f428b61de59703359197
Kelvin Wiriyatama <kelvin.wiriyatama88@gmail.com>
2022-05-26 22:43:05 +0700

    User API
    Make a user API for add user, edit user, get user, and delete user

---
+++ vepay-go-website-admin/functions/controller/user.js
@@ -0,0 +1,80 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+const userApp = express();
+
+userApp.use(cors({origin: true}));
+
+// get all users
+userApp.get("/users", async (req, res) => {
+  const snapshot = await db.collection("users").get();
+
+  const usersData = [];
+  snapshot.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
+
+    usersData.push({id, ...data});
+  });
+
+  res.status(200).send(JSON.stringify(usersData));
+});
+
+// get user by ID
+userApp.get("/users/:id", async (req, res) => {
+  const snapshot = await db.collection("users").doc(req.params.id).get();
+
+  const userId = snapshot.id;
+  const userData = snapshot.data();
+
+  if (!snapshot.exists) {
+    return res.status(404).send({"response": "User not found!"});
+  } else {
+    res.status(200).send(JSON.stringify({id: userId, ...userData}));
+  }
+});
+
+// register user
+userApp.post("/registration", (req, res) => {
+  const user = req.body;
+
+  db.collection("users").add(user);
+
+  res.status(201).send("Registration success!");
+});
+
+// edit user
+userApp.put("/users/:id", async (req, res) => {
+  const snapshot = await db.collection("users").doc(req.params.id).get();
+  const body = req.body;
+
+  if (!snapshot.exists) {
+    return res.status(404).send({"response": "User not found!"});
+  }
+  await db.collection("users").doc(req.params.id).update(body);
+  res.status(200).send("User profile updated!");
+});
+
+
+// detele user
+userApp.delete("/users/:id", async (req, res) => {
+  const snapshot = await db.collection("users").doc(req.params.id).get();
+
+  const userId = snapshot.id;
+  if (!snapshot.exists) {
+    return res.status(404).send({"response": "User not found!"});
+  }
+  await db.collection("users").doc(req.params.id).delete();
+
+  res.status(200).send(`User deleted with ID: ${userId}`);
+});
+
+
+exports.user = functions.https.onRequest(userApp);
