7a1d7069b69941a31e09961cfac20c8544171141
marcellinus-witarsah <aw.marcel@gmail.com>
2022-06-06 22:21:55 +0700

    adding changes to the MLModelAppEngine

--- MLmodelappengine/character_segmentation.py
+++ MLmodelappengine/character_segmentation.py
@@ -3,35 +3,37 @@ import numpy as np
 import functools
 from pathlib import Path
 import os
-import shutil
 import tensorflow as tf
 
 
-def detect_chars(imag, img_name='', showSteps=False):
-    # image = cv2.imread(imag)
-    # vis0 = cv2.fromarray(imag)
-    # img = cv2.imdecode(imag, flags=cv2.IMREAD_COLOR)
-    image = np.array(imag, np.uint8)
+def detect_chars(img, showSteps=False):
+    """
+    This function will receive an argument img which is an array of pixels which will be
+    processed and return array of character
+    """
+    image = np.array(img, np.uint8)
     image = cv2.resize(image, (300, 120))
-    # image = cv2.resize(imag, (120, 300))
-    # g = cv2.cv.fromarray(imag)
-    # g = np.asarray(g)
-    # image = cv2.resize(g,(300,120) )
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    pic_format = "resize.jpg"
+    path = os.path.join(Path().absolute(), "detected_chars")
+    cv2.imwrite(os.path.join(path, pic_format), image)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    pic_format = "resize_gray.jpg"
+    path = os.path.join(Path().absolute(), "detected_chars")
+    cv2.imwrite(os.path.join(path, pic_format), gray)
+
     gray = 255-gray
     gray = cv2.bilateralFilter(gray, 11, 17, 17)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.adaptiveThreshold(blurred, 255,
-                                   cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 43, 9)
-    thresh = cv2.erode(thresh, (3, 3))
-    thresh = cv2.dilate(thresh, (3, 3))
-    thresh = cv2.dilate(thresh, (3, 3))
-    thresh = cv2.erode(thresh, (3, 3))
+    thresh = cv2.adaptiveThreshold(
+        blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 21)
 
+    thresh = cv2.erode(thresh, (3, 3))
+    thresh = cv2.dilate(thresh, (3, 3))
+    thresh = cv2.dilate(thresh, (3, 3))
+    thresh = cv2.erode(thresh, (3, 3))
     _, labels = cv2.connectedComponents(thresh)
 
     mask = np.zeros(thresh.shape, dtype="uint8")
-
     # Set lower bound and upper bound criteria for characters
     total_pixels = image.shape[0] * image.shape[1]
     lower = total_pixels // 300  # heuristic param, can be fine tuned if necessary
@@ -80,72 +82,39 @@ def detect_chars(imag, img_name='', showSteps=False):
         y = y-2
         w = w+2
         h = h+2
-        if w > 5 and h > 10 and x > 2 and x < 280 and y > 10 and y < 60:
+        if w > 5 and h > 10 and x > 0 and x < 280 and y > 0 and y < 60:
             cv2.rectangle(new_mask, (x, y), (x+w, y+h), (0, 0, 0), 3)
             char = np.array(mask[y:y+h, x:x+w], np.uint8)
-            char = cv2.resize(char, (100, 75))
-            print(np.shape(char))
-            detected_char_list.append(char)
-            # detected_char_list.append(mask[y:y+h, x:x+w])
-    # cv2.imshow("detected_char",new_mask)
-    # output(detected_char_list, img_name)  # Take all detected_chars into a folder
+            char_resize = cv2.resize(char, (100, 75))
+            detected_char_list.append(char_resize)
+
     return detected_char_list
-    # return
 
-
-def output(detected_char_list, img_name):
-    i = 1
-    # Directory of current working directory, not __file__
-    path = os.path.join(Path().absolute(), "detected_chars")
-    path = os.path.join(path, img_name[:-3])
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    for pic in detected_char_list:
-        cv2.resize(pic, (70, 100))
-        print(pic)
-        pic_format = "test"+str(i)+".jpg"
-        cv2.imwrite(os.path.join(path, pic_format), pic)
-        i += 1
-
-
-def prepare_model():
-    imported_model = tf.keras.models.load_model('./binary_model_new_data.h5')
+# import the model
+def prepare_model(model_path):
+    """
+    This function will receive an argument model_path which is the path of .h5 model
+    """
+    imported_model = tf.keras.models.load_model(model_path)
     return imported_model
 
-
+# predict using the model and data
 def predict_characters(model, data):
+    """
+    This function will receive an argument model which will be used for predicting and 
+    data which is an array of characters that will be predicted by the model.
+    """
     characters = ''
-    # for char_file in os.listdir("detected_chars"):
-    #     img = cv2.imread("detected_chars/"+char_file)
-    #     # image_norm = cv2.normalize(img, None, alpha=0,beta=1, norm_type=cv2.NORM_MINMAX)
-    #     x = tf.convert_to_tensor(img)
-    #     x = tf.expand_dims(x, axis=0)
-
-    #     classes = model.predict(x)
-    #     result = int(np.argmax(classes))
-    #     dictionary = {0:'0', 1:'1', 2 :'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'A',
-    #         11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J', 20:'K',
-    #         21:'L', 22:'M', 23:'N', 24:'O', 25:'P', 26:'Q', 27:'R', 28:'S', 29:'T',
-    #         30:'U', 31:'V', 32:'W', 33:'X', 34:'Y', 35:'Z'}
-    #     print(dictionary[result])
-    #     characters += dictionary[result]
     dictionary = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A',
                   11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J', 20: 'K',
                   21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T',
                   30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'}
     for arr in data:
-        # img = cv2.imread("detected_chars/"+char_file)
-        # image_norm = cv2.normalize(img, None, alpha=0,beta=1, norm_type=cv2.NORM_MINMAX)
         x = tf.convert_to_tensor(arr)
         x = tf.expand_dims(x, axis=0)
-
-        classes = model.predict(x)
-        result = int(np.argmax(classes))
-        print(dictionary[result])
-        characters += dictionary[result]
-    # Delete Folder Directory
-    # if os.path.exists(os.path.join(os.getcwd(), "detected_chars")):
-    #     shutil.rmtree(os.path.join(os.getcwd(), "detected_chars"))
-
+        classes = model.predict(x, batch_size=10)
+        print(classes)
+        index = int(np.argmax(classes))
+        characters += dictionary[index]
+    
     return characters
--- MLmodelappengine/main.py
+++ MLmodelappengine/main.py
@@ -1,4 +1,3 @@
-from operator import mod
 from flask import Flask, jsonify
 from flask import request
 import numpy as np
@@ -6,19 +5,13 @@ from character_segmentation import detect_chars, predict_characters, prepare_mod
 
 app = Flask(__name__)
 
-@app.route('/test')
-def hello_world():
-   return 'Hello calvin'
-
-
-@app.route("/character-segmentation", methods=['POST'])
-def character_segmentation():
+@app.route("/", methods=['POST'])
+def index():
     data=request.get_json()
-    print("masuk data =\n", data["license-plate"])
+    # get the image which is in pixels
     np_data = np.array(data["license-plate"])
-    print(np_data.shape)
-    model = prepare_model()
-    data_for_prediction = detect_chars(imag=np_data)
+    model = prepare_model(model_path='./binary_model_multi_dropout.h5')
+    data_for_prediction = detect_chars(img=np_data)
     characters = predict_characters(model=model, data=data_for_prediction)
     print(characters)
     return jsonify(
